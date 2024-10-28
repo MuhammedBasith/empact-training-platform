cd70f1be5deccd261208915e56830466c02b8c8b
MuhammedBasith <iambasith123@gmail.com>
2024-10-28 12:43:48 +0530

    remove hoisting

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -15,6 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const auth = getAuth(app);
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
