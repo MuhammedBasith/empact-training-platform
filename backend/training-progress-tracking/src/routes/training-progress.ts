66430bf7b089c3fa46275e938239115924868912
MuhammedBasith <iambasith123@gmail.com>
2024-10-30 14:42:37 +0530

    update db schema

--- backend/training-progress-tracking/src/routes/training-progress.ts
+++ backend/training-progress-tracking/src/routes/training-progress.ts
@@ -1,11 +1,16 @@
-import {  createTrainingProgress, getAllTrainingProgress, getTrainingProgressById, updateTrainingProgress } from "controller/training-progress";
+import {
+    createTrainerFeedbackWithProgress,
+    getAllTrainerFeedbackWithProgress,
+    getTrainerFeedbackWithProgressById,
+    updateTrainerFeedbackWithProgress
+} from "../controllers/training-progress";
 import { Router } from "express";
 
 const routes=Router();
 
-routes.post('/',createTrainingProgress);
-routes.get('/',getAllTrainingProgress);
-routes.get('/:id', getTrainingProgressById);
-routes.put('/:id', updateTrainingProgress );
+routes.post('/',createTrainerFeedbackWithProgress);
+routes.get('/',getAllTrainerFeedbackWithProgress);
+routes.get('/:id', getTrainerFeedbackWithProgressById);
+routes.put('/:id', updateTrainerFeedbackWithProgress );
 
 export default routes;
