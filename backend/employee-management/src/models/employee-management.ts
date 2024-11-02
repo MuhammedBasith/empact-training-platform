427de796ef1070588c3f452be7a118a2a156fe1b
rapoluprashanth <rapoluprashanth123@gmail.com>
2024-11-02 22:56:04 +0530

    added put method to update the trainerId for employee

--- backend/employee-management/src/models/employee-management.ts
+++ backend/employee-management/src/models/employee-management.ts
@@ -2,12 +2,11 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IEmployeeManagement extends Document{
     cognitoId: mongoose.Types.ObjectId;
-    userId: number;
     empName: string,
     empEmail: string,
     empAccount: string,
     empSkills: string,
-    trainingIds: number;
+    trainingIds: mongoose.Types.ObjectId | null;
     department: string;
     role: 'employee';
     hiredAt: Date;
@@ -15,12 +14,11 @@ export interface IEmployeeManagement extends Document{
 
 const EmployeeManagementSchema: Schema<IEmployeeManagement> = new Schema({
   cognitoId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
-    userId: { type: Number, ref: 'users', required: true },
     empName: {type: String, required:true},
     empEmail: {type: String, required: true},
     empAccount: {type: String, required: true},
     empSkills: {type: String, required: true},
-    trainingIds: { type:Number },
+    trainingIds: { type: Schema.Types.ObjectId, ref: 'trainingRequirements' },
     department: { type: String, required: true },
     role: { type: String, enum: ['employee'], required: true },
     hiredAt: { type: Date }
