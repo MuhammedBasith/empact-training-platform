14b4c50f89ac1f7f84351082e76e1d1d074259a4
rapoluprashanth <rapoluprashanth123@gmail.com>
2024-11-04 12:20:07 +0530

    changed url from trainer to trainers

--- backend/trainer-management/src/routes/trainer-management.ts
+++ backend/trainer-management/src/routes/trainer-management.ts
@@ -4,7 +4,7 @@ import { Router } from "express";
 const router=Router();
 
 router.post('/trainer',createTrainer);
-router.get('/trainer', getAllTrainers);
+router.get('/trainers', getAllTrainers);
 router.get('/trainer/:id',getTrainerById);
 router.put('/trainer/:id', updateTrainer);
 router.delete('/trainer/:id', deleteTrainer);
