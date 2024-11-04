3dc76de9ab28822b2ab6325f4577c67a0401b0f1
MuhammedBasith <iambasith123@gmail.com>
2024-11-04 10:54:38 +0530

    update batch and trainerfeedback schema

--- backend/batch-management/src/models/batchModel.ts
+++ backend/batch-management/src/models/batchModel.ts
@@ -11,7 +11,7 @@ export interface IBatch extends Document {
 const BatchSchema: Schema<IBatch> = new Schema({
     trainingRequirementId: { type: Schema.Types.ObjectId, required: true, ref: 'TrainingRequirement' },
     batchNumber: { type: Number, required: true },
-    trainerId: { type: Schema.Types.ObjectId, required: true, ref: 'Trainer' },
+    trainerId: { type: Schema.Types.ObjectId, ref: 'Trainer' },
     employeeIds: [{ type: Schema.Types.ObjectId, ref: 'EmployeeManagement' }],
     createdAt: { type: Date, default: Date.now },
 });
