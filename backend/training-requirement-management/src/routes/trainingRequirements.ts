c5c54b6891b9054c460a094ae09fec72f15c2029
rapoluprashanth <rapoluprashanth123@gmail.com>
2024-11-04 21:23:01 +0530

    added traningName to get in get method

--- backend/training-requirement-management/src/routes/trainingRequirements.ts
+++ backend/training-requirement-management/src/routes/trainingRequirements.ts
@@ -1,18 +1,19 @@
 import express from 'express';
-import { createTrainingRequirement, getTrainingRequirement, getAllTrainingRequirements, updateEmployeeCount, deleteTrainingRequirement, confirmRequirement, updateBatchIds, getTrainingRequirementsByManager, getTrainingRequirements } from '../controllers/trainingRequirementsController';
+import { createTrainingRequirement, getTrainingRequirement, getAllTrainingRequirements, updateEmployeeCount, deleteTrainingRequirement, confirmRequirement, updateBatchIds, getTrainingRequirementsByManager, getTrainingRequirements, getEmpCountById } from '../controllers/trainingRequirementsController';
 import { CreateTrainingRequirementDto, UpdateTrainingRequirementDto } from '../dtos/trainingRequirements.dto';
 
 const router = express.Router();
 
 router.post('/', createTrainingRequirement);
 router.post('/:requirementId/confirm', confirmRequirement);
- router.get('/:cognitoId/:id', getTrainingRequirement);
+ router.get('/getTrainingRequirement/:cognitoId/:id', getTrainingRequirement);
 // router.get('/', getAllTrainingRequirements);
 router.put('/:id/empCount', updateEmployeeCount);
 router.put('/:id/batchIds',updateBatchIds)
 router.delete('/:id', deleteTrainingRequirement);
-router.get('/:id',getTrainingRequirementsByManager);
+router.get('/getTrainingRequirementsByManager/:id',getTrainingRequirementsByManager);
 router.get('/',getTrainingRequirements)   // cognitoId, name & count
+router.get('/getEmpCountById/:id',getEmpCountById);
 
 
 export default router;
