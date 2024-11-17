c8c83b18ef31fdfb7fa2b5ded3335060c9b1b9ad
MuhammedBasith <iambasith123@gmail.com>
2024-11-17 20:05:19 +0530

    remove redundant imports

--- backend/authentication/src/server.ts
+++ backend/authentication/src/server.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import helmet from 'helmet';
 import cors from 'cors';
-import morgan from 'morgan';
 import { connectToDatabase } from '../../authentication/src/config/db.config';
 import authRoutes from './routes/auth.routes';
 import 'dotenv/config'
@@ -10,20 +8,13 @@ import 'dotenv/config'
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Middleware
-app.use(helmet()); // For security headers
 app.use(cors()); // Enable CORS
-app.use(morgan('dev')); // For request logging in the console
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/auth', authRoutes);
 
-// Health check route
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('API is up and running!');
-});
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
