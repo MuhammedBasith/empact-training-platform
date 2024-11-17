39d396b33040c07723631a3d19ab2772e6f48e8f
empact-platform <empactplatform@gmail.com>
2024-11-17 18:02:47 +0530

    Update server.ts

--- backend/batch-management/src/server.ts
+++ backend/batch-management/src/server.ts
@@ -1,8 +1,23 @@
-import app from './app';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/database';
 import BatchRoutes from './routes/batchRoutes';
+import errorHandler from './middlewares/errorHandler';
 import 'dotenv/config';
 
+
 const PORT = process.env.PORT || 3009;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+app.use('/api/v1/batch-management', BatchRoutes)
+
+
+app.use(errorHandler);
 
 
 app.listen(PORT, () => {
